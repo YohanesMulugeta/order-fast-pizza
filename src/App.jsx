1ea34ce79d0d0aa698b50bb0d830ba5216bc601b
Yohanes <yohanesmulugeta21@gmail.com>
2023-12-14 17:57:17 +0300

    implement action performs on the CreateOrder component and link to the route in the App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, { action as createOrderAction } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 
 const router = createBrowserRouter([
@@ -16,7 +16,12 @@ const router = createBrowserRouter([
       { path: '/', element: <Home /> },
       { path: '/menu', element: <Menu />, loader: menuLoader, errorElement: <Error /> },
       { path: '/cart', element: <Cart /> },
-      { path: '/order/new', element: <CreateOrder /> },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+        action: createOrderAction,
+        errorElement: <Error />,
+      },
       {
         path: '/order/:orderId',
         element: <Order />,
