430187400812be256597bd6f6f916e5a36863325
Yohanes <yohanesmulugeta21@gmail.com>
2023-12-14 11:43:12 +0300

    implement Layout component and make it the parent of all routes.

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,20 @@ import Home from './ui/Home';
 import Menu from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
+import AppLayout from './ui/AppLayout';
 // import Order from './features/order/Order';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/menu', element: <Menu /> },
-  { path: '/cart', element: <Cart /> },
-  { path: '/order/new', element: <CreateOrder /> },
-  // { path: '/order/:orderId', element: <Order /> },
+  {
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/menu', element: <Menu /> },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order/new', element: <CreateOrder /> },
+      // { path: '/order/:orderId', element: <Order /> },
+    ],
+  },
 ]);
 
 function App() {
