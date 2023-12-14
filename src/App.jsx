ec8cf4d4178b10cec462cba16d2a18683660bf52
Yohanes <yohanesmulugeta21@gmail.com>
2023-12-14 14:44:41 +0300

    incoporate error element to display errors.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router';
 
 import Home from './ui/Home';
+import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
-import AppLayout from './ui/AppLayout';
 // import Order from './features/order/Order';
 
 const router = createBrowserRouter([
@@ -13,7 +14,7 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: '/', element: <Home /> },
-      { path: '/menu', element: <Menu />, loader: menuLoader },
+      { path: '/menu', element: <Menu />, loader: menuLoader, errorElement: <Error /> },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
       // { path: '/order/:orderId', element: <Order /> },
