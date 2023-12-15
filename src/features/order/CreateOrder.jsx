641bf7d35cd79645b91d264746ce15d34a3e25e4
Yohanes <yohanesmulugeta21@gmail.com>
2023-12-15 21:29:44 +0300

    add prettier.config.js to configure prettier-plugin-tailwindcss.

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { Form, redirect, useNavigation } from 'react-router-dom';
+// import { useState } from 'react';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
@@ -34,8 +34,10 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-
   const isSubmitting = navigation.state === 'submitting';
+
+  const formErrors = useActionData();
+
   // const [withPriority, setWithPriority] = useState(false);
 
   return (
@@ -54,6 +56,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
+          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
         <div>
@@ -94,6 +97,15 @@ export async function action({ request }) {
     priority: data.priority === 'on',
   };
 
+  // Error Handling
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      'Please give us the correct phone number, we might need to contact you.';
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay create order then redirect
   const newOrder = await createOrder(order);
 
   return redirect(`/order/${newOrder.id}`);
